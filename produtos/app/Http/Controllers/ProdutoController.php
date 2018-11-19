3b6a3460fc53edd4d0a18b48a46a66ab88b0d4f5
AdrielBento <adrielbb15@gmail.com>
2018-11-18 22:37:17 -0200

    Implements methods to remove and update Produto

--- produtos/app/Http/Controllers/ProdutoController.php
+++ produtos/app/Http/Controllers/ProdutoController.php
@@ -25,11 +25,22 @@ class ProdutoController extends Controller{
     }
 
     public function updateProduto(ProdutoRequest $request){
-
+        $produto = Produto::find($id);
+        return response()->json(['produto' => $produto]);
     }
 
     public function removeProduto(ProdutoRequest $request){
 
+        $produto = Produto::find($id);
+        $status = true;
+
+        try {
+            $produto->delete();
+        } catch (\Throwable $th) {
+            $status = false;
+        }
+
+        return response()->json(['status' => $status]);
     }
 
 
