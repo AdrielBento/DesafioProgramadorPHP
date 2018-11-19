21834fadd1354aaebff782d273285ce7d56546c8
AdrielBento <adrielbb15@gmail.com>
2018-11-18 22:29:25 -0200

    Creates blade pages for reuse

---
+++ produtos/resources/views/layout/head.blade.php
@@ -0,0 +1,8 @@
+@section('content')
+<link href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" rel="stylesheet">
+<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" crossorigin="anonymous">
+<link rel="stylesheet" href="styles/geral.css">
+<link rel="stylesheet" id="main-stylesheet" data-version="1.1.0" href="styles/shards-dashboards.1.1.0.css">
+
+@stop
---
+++ produtos/resources/views/layout/sidebar.blade.php
@@ -0,0 +1,34 @@
+<div class="main-navbar">
+   <nav class="navbar align-items-stretch navbar-light bg-white flex-md-nowrap border-bottom p-0">
+     <a class="navbar-brand w-100 mr-0" href="Gerente" style="line-height: 25px;">
+       <div class="d-table m-auto">
+         <img id="main-logo" class="d-inline-block align-top mr-1" style="width: 60px;" src="img/Beibe.svg" alt="Shards Dashboard">
+       </div>
+     </a>
+     <a class="toggle-sidebar d-sm-inline d-md-none d-lg-none">
+       <i class="material-icons">&#xE5C4;</i>
+     </a>
+   </nav>
+ </div>
+ <div class="nav-wrapper">
+    <ul class="nav flex-column">
+      <li class="nav-item">
+        <a class="nav-link " href="Gerente?action=getUsuarios">
+            <i class="fas fa-chart-pie fa-lg"></i>
+            <span>Dashboard</span>
+        </a>
+     </li>
+     <li class="nav-item">
+        <a class="nav-link active" href="Gerente?action=listFuncionarioGerente">
+            <i class="fas fa-users fa-lg"></i>
+            <span>Usuarios</span>
+        </a>
+     </li>
+     <li class="nav-item">
+        <a class="nav-link active " href="Gerente?action=getAtendimentos">
+        <i class="fas fa-headset"></i>
+            <span>Atendimentos</span>
+        </a>
+     </li>
+   </ul>
+ </div>
