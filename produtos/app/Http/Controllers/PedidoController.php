fca9f97f859fe1d7831319d12a00d9e176ef0648
AdrielBento <adrielbb15@gmail.com>
2018-11-19 00:24:46 -0200

    Implements method to add a Pedido

--- produtos/app/Http/Controllers/PedidoController.php
+++ produtos/app/Http/Controllers/PedidoController.php
@@ -5,14 +5,43 @@ use Illuminate\Support\Facades\DB;
 use Request;
 use produtos\Produto;
 use Validator;
+use DB;
 // use estoque\Http\Requests\ProdutoRequest;
 
 class ProdutoController extends Controller{
 
 
-    public function addProduto(Request $request){
-        // $params = $request->all();
-        // $produto = new Produto($params);
+    public function addPedido(PedidoRequest $request){
+
+        $params = $request->all();
+        $dataAtual = new DateTime();
+        $itensPedido = $idsProdutos = Array();
+
+        foreach ($params as $key => $value) {
+
+        }
+
+        $precosProdutos = DB::table("tb_produto")->select("id,preco")->whereIn('id', [$idsProdutos])->get();
+
+        $total = array_reduce($precosProdutos, function($preco, $produto) {
+            $preco += $produto['preco'];
+            return $preco;
+        });
+
+
+        try {
+
+            $idPedido = DB::table("tb_pedido")->insertGetId(["total"=>$total,"data"=>$dataAtual]);
+
+            foreach ($variable as $key => $value) {
+                $itensPedido[] = ["idProduto"=>$value,"idPedido"=>$idPedido,"quantidade"=>$value];
+            }
+
+            DB::table("tb_itens_pedido")->insert([$itensPedido]);
+        } catch (\Throwable $th) {
+
+        }
+        
         Produto::create($request->all());
         $produto->save();
     }
